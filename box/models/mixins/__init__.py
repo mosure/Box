bff0be21b7b8d9f5566fa2aa93d95644350cb430
Brady Lenz <bradylenz98@gmail.com>
2019-06-23 19:22:44 -0400

    added OwnerMixin

--- box/models/mixins/__init__.py
+++ box/models/mixins/__init__.py
@@ -2,3 +2,4 @@ from .chronoedge import *
 from .completable import *
 from .idable import *
 from .metadatable import *
+from .owner import *
---
+++ box/models/mixins/owner.py
@@ -0,0 +1,4 @@
+class OwnerMixin():
+    def __init__(self, jobs, parts):
+        self.jobs = jobs
+        self.parts = parts
